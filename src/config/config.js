87cb2ea7806eadf811ad5b82c1f43483dfc9009d
Maria Rosales <thelife231@gmail.com>
2019-04-21 17:33:50 -0500

    make

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,24 @@
 process.env.PORT=process.env.PORT || 3000;
-// process.env.NODE_ENV=process.env.PORT.NODE_ENV || "heroku";
-// let URLDB
-// console.log(process.env.NODE_ENV);
-// if (process.env.NODE_ENV === 'local'){
-// //Agregar el puerto sobre el cual escucha mongodb
-//     URLDB = 'mongodb://localhost:27017/cursosplataformavirtual';
-// }
-// else {
-//     URLDB = 'mongodb+srv://admin:<1234>@nodejstdea-1cqmw.mongodb.net/cursosplataformavirtual?retryWrites=true';
-// }
+ process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
+ let URLDB
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'local'){
+//Agregar el puerto sobre el cual escucha mongodb
+    URLDB = 'mongodb://localhost:27017/cursosplataformavirtual';
+}
+else {
+    require('dotenv').config()
+    URLDB = 'mongodb+srv://admin:<1234>@nodejstdea-1cqmw.mongodb.net/cursosplataformavirtual?retryWrites=true';
+}
 
-// process.env.URLDB = URLDB
+process.env.URLDB = URLDB
 
 if(!process.env.URLDB){
 
     process.env.URLDB = 'mongodb://localhost:27017/cursosplataformavirtual'
     
     }
+// if(process.env.NODE_ENV !=='local'){
+//     require('dotenv').config();
+//     process.env.URLDB = 'mongodb://localhost:27017/cursosplataformavirtual'
+// }
