af85bee30fcb538f8337aa51fb4bac37ad11661a
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-20 21:59:31 -0500

    con atlas

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 process.env.PORT=process.env.PORT || 3000;
 process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
-let urlDB
+let URLDB
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'local'){
 //Agregar el puerto sobre el cual escucha mongodb
-    urlDB = 'mongodb://localhost:27017/cursosplataformavirtual';
+    URLDB = 'mongodb://localhost:27017/cursosplataformavirtual';
 }
 else {
-    urlDB = 'mongodb+srv://juancamilo:<1234>@cursoplataformavirtual-lkkto.mongodb.net/test?retryWrites=true';
+    URLDB = 'mongodb+srv://devtimeadmin:<BeyondDevTime>@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 }
 
-process.env.URLDB = urlDB
+process.env.URLDB = URLDB
 
