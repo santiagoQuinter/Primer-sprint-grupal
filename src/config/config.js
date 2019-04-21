9a69e662442aa7c6a5196552f2c26ce8a968d657
Maria Rosales <thelife231@gmail.com>
2019-04-20 21:50:01 -0500

    make it better

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,14 @@
 process.env.PORT=process.env.PORT || 3000;
-process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
-let urlDB
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'local'){
-//Agregar el puerto sobre el cual escucha mongodb
-    urlDB = 'mongodb://localhost:27017/cursosplataformavirtual';
-}
-else {
-    urlDB = 'mongodb+srv://admin:<1234>@nodejstdea-1cqmw.mongodb.net/cursosplataformavirtual?retryWrites=true';
-}
+// process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
+// let urlDB
+// console.log(process.env.NODE_ENV);
+// if (process.env.NODE_ENV === 'local'){
+// //Agregar el puerto sobre el cual escucha mongodb
+//     urlDB = 'mongodb://localhost:27017/cursosplataformavirtual';
+// }
+// else {
+//     urlDB = 'mongodb+srv://admin:<1234>@nodejstdea-1cqmw.mongodb.net/cursosplataformavirtual?retryWrites=true';
+// }
 
-process.env.URLDB = urlDB
-
+// process.env.URLDB = urlDB
+process.env.URLDB ='mongodb://localhost:27017/cursosplataformavirtual'
