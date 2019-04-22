aae3568ca4a1d5aa663104292d13f4032678a041
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-21 23:01:50 -0500

    ppppp

--- src/config/config.js
+++ src/config/config.js
@@ -13,9 +13,7 @@ else {
     URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 }
 
-if(NODE_ENV === 'production'){
-	URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
-}
+
 	
 
 
