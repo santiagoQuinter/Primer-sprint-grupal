39bb1e600a363ecf1c48b1f73dd6579b8f3d0c67
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-21 20:07:49 -0500

    aaaa

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 process.env.PORT=process.env.PORT || 3000;
 process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
 let URLDB
+URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'local'){
 //Agregar el puerto sobre el cual escucha mongodb
