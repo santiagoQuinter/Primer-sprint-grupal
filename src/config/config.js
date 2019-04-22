a7fde4e70682f1370e85f5b24b4f6d781ddafaf5
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-21 19:46:27 -0500

    aaaaaaaaaaaaa

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 process.env.PORT=process.env.PORT || 3000;
 process.env.NODE_ENV=process.env.PORT.NODE_ENV || "local";
 let URLDB
-URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/cursosplataformavirtual?retryWrites=true';
+URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true'
 console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'local'){
 //Agregar el puerto sobre el cual escucha mongodb
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'local'){
 }
 else {
     require('dotenv').config()
-    URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/cursosplataformavirtual?retryWrites=true';
+    URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 }
 
 process.env.URLDB = URLDB
