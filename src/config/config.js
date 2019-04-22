73f08205193439bc7a68480f8a796b41b8b30ae7
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-21 22:53:40 -0500

    nnnnn

--- src/config/config.js
+++ src/config/config.js
@@ -13,10 +13,8 @@ else {
     URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 }
 
-if(!process.env.NODE_ENV){
-	URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
-}
-	
+
+URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true';
 
 process.env.URLDB = URLDB
 
