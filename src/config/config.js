aaec53aad7c6da26d415373e14b67c18e47512f8
santiagoQuinter <saquinteros@unal.edu.co>
2019-04-22 19:13:51 -0500

    sin setear variable en el package

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 process.env.PORT=process.env.PORT || 3000;
 process.env.NODE_ENV=process.env.NODE_ENV || "local";
 let URLDB
-URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true'
-console.log("desde config con PORT: " +process.env.PORT.NODE_ENV);
-console.log("desde config: " + process.env.NODE_ENV);
+//URLDB = 'mongodb+srv://devtimeadmin:BeyondDevTime@divetimedatabase-arktg.mongodb.net/test?retryWrites=true'
+//console.log("desde config con PORT: " +process.env.PORT.NODE_ENV);
+//console.log("desde config: " + process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'local'){
 //Agregar el puerto sobre el cual escucha mongodb
     URLDB = 'mongodb://localhost:27017/cursosplataformavirtual';
